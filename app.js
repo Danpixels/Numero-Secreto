40cb017dfac94941c5bcda6e9967a286ef8caf4f
Danpixels <danypixls66@gmail.com>
2024-12-12 21:21:01 -0300

    Joguinho Completo!

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+let numeroMaximo = 100
 alert("Boas vindas ao jogo do número secreto!");
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random () * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1
 
 //enquanto chute não for igual ao numeroSecreto
 while (chute != numeroSecreto) {
-    chute = prompt("Escolha um número entre 1 e 10");
+    chute = prompt(`Escolhe um número de 1 a ${numeroMaximo}`);
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert (`O número secreto é menor que ${chute}`);
@@ -21,4 +22,6 @@ while (chute != numeroSecreto) {
     }
 }
 
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
